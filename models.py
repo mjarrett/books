15ce40fec4de431aac9b089d7cb02cb801550cc7
Mike Jarrett <msjarrett@gmail.com>
2017-01-25 21:28:12 -0800

    Adds list sorting in views and author page

--- models.py
+++ models.py
@@ -7,15 +7,15 @@ from django.forms import Form, ModelForm, CharField, PasswordInput
 
 class Location(models.Model):
     location = models.CharField(max_length=200, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, related_name='location')
 
     def __str__(self):
         return self.location
 
 class Book(models.Model):
 
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, related_name='book')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='book')
     title = models.CharField(max_length=200, null=True)
     isbn = models.IntegerField(default=-1)
     author = models.CharField(max_length=200,null=True)
@@ -29,7 +29,7 @@ class Book(models.Model):
 
 class Category(models.Model):
     category = models.CharField(max_length=200, unique=True)
-    book = models.ManyToManyField(Book)
+    book = models.ManyToManyField(Book,related_name='category')
 
     def __str__(self):
         return self.category
